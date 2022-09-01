dea836510935b63ce8ffa3394d5ccf9c10b7cd6b
Sam Askling <samaskling@gmail.com>
2022-09-01 03:52:50 -0500

    Remove debug guild

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -5,5 +5,5 @@ intents = discord.Intents().default()
 intents.members = True
 intents.guilds = True
 
-bot = commands.Bot(description="Make threads better!", intents=intents, help_command=None, case_insensitive=True, strip_after_prefix=True, debug_guilds=[764385563289452545])
+bot = commands.Bot(description="Make threads better!", intents=intents, help_command=None, case_insensitive=True, strip_after_prefix=True)
 bot.owner_id = 243759220057571328
